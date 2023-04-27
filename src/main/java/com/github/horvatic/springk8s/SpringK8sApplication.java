267905d5e469454ffae34315ec46a128a324684a
horvatic <sbhorvatic@gmail.com>
2023-04-27 15:07:29 +0000

    init commit

---
+++ src/main/java/com/github/horvatic/springk8s/SpringK8sApplication.java
@@ -0,0 +1,19 @@
+package com.github.horvatic.springk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringK8sApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(SpringK8sApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
---
+++ test/main/java/com/github/horvatic/springk8s/SpringK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.horvatic.springk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringK8sApplication {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
