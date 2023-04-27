1cd7b838794fa0331a33634fff792d8b3e9c56e9
horvatic <sbhorvatic@gmail.com>
2023-04-27 18:50:12 +0000

    add health check

--- src/main/java/com/github/horvatic/springk8s/SpringK8sApplication.java
+++ src/main/java/com/github/horvatic/springk8s/SpringK8sApplication.java
@@ -16,4 +16,9 @@ public class SpringK8sApplication {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
+
+    @GetMapping("/health")
+    public String health() {
+      return String.format("OK");
+    }
 }
