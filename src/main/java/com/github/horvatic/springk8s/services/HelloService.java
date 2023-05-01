4e6597c3438628fb3f87114ed72eff8130f84c11
horvatic <sbhorvatic@gmail.com>
2023-05-01 22:35:49 +0000

    add remove info

--- src/main/java/com/github/horvatic/springk8s/services/HelloService.java
+++ src/main/java/com/github/horvatic/springk8s/services/HelloService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService implements IHelloService {
     public String getHello(String name) {
-        return String.format("Hello %s, Welcome!!!!", name);
+        return String.format("Hello %s, Welcome!", name);
     }
 }
